ba5fc94d866c7540230a7d451ac096e2e600e3e8
Marcin Wojciechowski <kierej@gmail.com>
2023-09-27 17:57:35 +0200

    rmeoved all async from Blog page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,21 +1,7 @@
-async function getData() {
-  const endpoint = "http://localhost:3000/api/playground";
-  const res = await fetch(endpoint);
-  console.log("res", res);
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
-
 interface Props {}
 
-const BlogPage: React.FC<Props> = async () => {
-  const data = await getData();
-
-  console.log("data", data);
-  return <div>{JSON.stringify(data)}</div>;
+const BlogPage: React.FC<Props> = () => {
+  return <div>SIEMBA BLOK</div>;
 };
 
 export default BlogPage;
