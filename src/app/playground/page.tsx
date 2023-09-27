81dabd9b2e04db5377004edbde2651364b5e4794
Marcin Wojciechowski <kierej@gmail.com>
2023-09-27 17:51:02 +0200

    remove fetch from Blog

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,18 +1,14 @@
-"use client";
-
-import { useEffect } from "react";
-
 interface Props {}
 
 const BlogPage: React.FC<Props> = () => {
-  useEffect(() => {
-    fetch("http://localhost:3000" + "/api/playground    ", {
-      method: "POST",
-      body: JSON.stringify({
-        product: "Product Siema",
-      }),
-    }).catch((error) => console.log("error", error));
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:3000" + "/api/playground    ", {
+  //     method: "POST",
+  //     body: JSON.stringify({
+  //       product: "Product Siema",
+  //     }),
+  //   }).catch((error) => console.log("error", error));
+  // }, []);
 
   return <div>Siema</div>;
 };
