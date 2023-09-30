aac37cde8c9633b4b0bcb976df5c5956e3a68783
Szymon Wojciechowski <kierej@gmail.com>
2023-09-30 18:07:05 +0200

    try neon db connection - PRACTISE 1

--- src/app/neon/page.tsx
+++ src/app/neon/page.tsx
@@ -1,14 +1,13 @@
-import { helloWorldDb } from "../../lib/db";
+import { helloWorldDb } from "@/lib/db";
 
 export default async function NeonConnectionPage() {
-  const elo = await helloWorldDb();
-
+  const db = await helloWorldDb();
+  console.log("db", db);
   return (
     <div>
-      NeonConnectionPage<div>{elo.latency}ms</div>
+      NeonConnectionPage<div>{db.latency}ms</div>
     </div>
   );
 }
 
-export const runtime = "edge";
 export const revalidate = 0;
