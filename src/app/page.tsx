d1841611d1d9f614de1deca7c6181b75331654ac
Szymon Wojciechowski <kierej@gmail.com>
2023-10-05 18:31:12 +0200

    userId

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Session of the userId: {userId}
+      Session of the userId: {userId as string}
     </main>
   );
 }
